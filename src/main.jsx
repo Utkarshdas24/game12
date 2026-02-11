20b72528d0c0a19469802258a77df0a55e40f17f
Diwakar Adhikari/Pune Ho /Inhouse Sales/Life <Diwakar.Adhikari01@bajajlife.com>
2026-02-11 16:49:01 +0530

    Fix asset paths and improve sound handling in various components

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+console.log('UAT DEPLOYMENT DATE: 11th Feb 2026 , 16:48');
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
